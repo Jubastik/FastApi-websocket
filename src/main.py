8832da7b87c1b43d73a8b19f67dfd5c4db61989e
Artem <artem00561@gmail.com>
2023-12-12 21:54:24 +0300

    add(base) Добавлена архитектура приложения, его зависимости, а также базовый шаблон вебсокета

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from pages.router import router as router_pages
+from chat.router import router as router_chat
+
+app = FastAPI(
+    title="Trading App"
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(router_pages)
+app.include_router(router_chat)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
